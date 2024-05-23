eb85dfe32dbe1c82e7dac8a4ac8515cf803179e3
supersliser <thomaslower9@gmail.com>
2024-05-23 21:15:01 +0100

    fdsfs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx(), react()],
+  output: "server",
+  adapter: vercel()
+});
---
+++ src/components/CodeBlock.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function CodeBlock({ children }) {
+    const [visible, setVisible] = useState(false);
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <button style={{float: "right"}} onClick={() => setVisible(!visible)}>{visible ? "Hide" : "Show"} Code</button>
+            {visible && <div style={{fontSize: "0.6em"}}>{children}</div>}
+        </div>
+    );
+}
